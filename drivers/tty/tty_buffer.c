123855aa2db1a8a344e84d530e071294fcfb791d
Fiqri Ardyansyah <fiqri15072019@gmail.com>
2020-09-09 15:17:15 +0700

    drivers: tty: kthread worker API cleanup
    queue_kthread_work()    -> kthread_queue_work()
    init_kthread_work()     -> kthread_init_work()
    init_kthread_worker()   -> kthread_init_worker()
    flush_kthread_worker()  -> kthread_flush_worker()
    
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>
    Signed-off-by: wimbiyoas <wyoashizkia@gmail.com>
    Signed-off-by: Thoreck <maulanafajar751@gmail.com>

--- drivers/tty/tty_buffer.c
+++ drivers/tty/tty_buffer.c
@@ -71,7 +71,7 @@ void tty_buffer_unlock_exclusive(struct tty_port *port)
 	atomic_dec(&buf->priority);
 	mutex_unlock(&buf->lock);
 	if (restart)
-		queue_kthread_work(&port->worker, &buf->work);
+		kthread_queue_work(&port->worker, &buf->work);
 }
 EXPORT_SYMBOL_GPL(tty_buffer_unlock_exclusive);
 
@@ -406,7 +406,11 @@ void tty_schedule_flip(struct tty_port *port)
 	 * flush_to_ldisc() sees buffer data.
 	 */
 	smp_store_release(&buf->tail->commit, buf->tail->used);
+<<<<<<< HEAD
 	queue_kthread_work(&port->worker, &buf->work);
+=======
+	kthread_queue_work(&port->worker, &buf->work);
+>>>>>>> 5df20e09ff20 (drivers: tty: kthread worker API cleanup)
 }
 EXPORT_SYMBOL(tty_schedule_flip);
 
@@ -565,8 +569,13 @@ void tty_buffer_init(struct tty_port *port)
 	atomic_set(&buf->mem_used, 0);
 	atomic_set(&buf->priority, 0);
 	buf->mem_limit = TTYB_DEFAULT_MEM_LIMIT;
+<<<<<<< HEAD
 	init_kthread_work(&buf->work, flush_to_ldisc);
 	init_kthread_worker(&port->worker);
+=======
+	kthread_init_work(&buf->work, flush_to_ldisc);
+	kthread_init_worker(&port->worker);
+>>>>>>> 5df20e09ff20 (drivers: tty: kthread worker API cleanup)
 	port->worker_thread = kthread_run(kthread_worker_fn, &port->worker,
 					  "tty_worker_thread");
 	if (IS_ERR(port->worker_thread)) {
@@ -577,7 +586,10 @@ void tty_buffer_init(struct tty_port *port)
 		pr_err("Unable to start tty_worker_thread\n");
 	}
 
+<<<<<<< HEAD
 
+=======
+>>>>>>> 5df20e09ff20 (drivers: tty: kthread worker API cleanup)
 }
 
 /**
@@ -605,7 +617,11 @@ void tty_buffer_set_lock_subclass(struct tty_port *port)
 
 bool tty_buffer_restart_work(struct tty_port *port)
 {
+<<<<<<< HEAD
 	return queue_kthread_work(&port->worker, &port->buf.work);
+=======
+	return kthread_queue_work(&port->worker, &port->buf.work);
+>>>>>>> 5df20e09ff20 (drivers: tty: kthread worker API cleanup)
 }
 
 bool tty_buffer_cancel_work(struct tty_port *port)
@@ -615,5 +631,9 @@ bool tty_buffer_cancel_work(struct tty_port *port)
 
 void tty_buffer_flush_work(struct tty_port *port)
 {
+<<<<<<< HEAD
 	flush_kthread_work(&port->buf.work);
+=======
+	kthread_flush_work(&port->buf.work);
+>>>>>>> 5df20e09ff20 (drivers: tty: kthread worker API cleanup)
 }
